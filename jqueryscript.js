d7e5057393269d40107db6bd9240030ff9ba37e6
samprit <goudasamprit@gmail.com>
2025-09-08 13:51:06 +0530

    chnages all class names

--- jqueryscript.js
+++ jqueryscript.js
@@ -3,13 +3,13 @@
 $(document).ready(()=>{
 
     //changing the background color for all sections
-    $(".change-bg").addClass("highlight-bg")
+    $(".section-bg").addClass("highlight-bg")
 
     //changing the border color #FF1A00 and adding borders to some sections
-    $(".change-border").addClass("highlight-border")
+    $(".section-border").addClass("highlight-border")
     
     //changing the font color to #CC0000
-    $(".change-text").addClass("highlight-text")
+    $(".section-text").addClass("highlight-text")
 
     //removing the border for all input elements
     $(".form-input").addClass("remove-border")
